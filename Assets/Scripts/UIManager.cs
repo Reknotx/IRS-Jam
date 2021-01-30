ce60f6f322bb30f8270d131c26e44ab1fc75c49e
Chase O'Connor <chaseoconnor3@comcast.net>
2021-01-29 23:45:13 -0800

    ui manager update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _score;
+    private float _secondsLeft;
+
+    public int maxScore;
+
+    
+    public int minScoreRange;
+    
+    [Header("Max score range MUST be larger than min score range.")]
+    public int maxScoreRange;
+
+    public float SecondsLeft
     {
-        
+        get => _secondsLeft;
+        set
+        {
+            _secondsLeft = value;
+            
+            if (timeText != null)
+            {
+                float minutes = _secondsLeft / 60;
+                float seconds = _secondsLeft % 60;
+
+                if (seconds < 10)
+                    timeText.text = "Time remaining: " + minutes + ":0" + seconds;
+                else
+                    timeText.text = "Time remaining: " + minutes + ":" + seconds;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Score
     {
-        
+        get => _score;
+        set
+        {
+            _score += value;
+            
+            if (scoreText != null)
+            {
+                scoreText.text = "Score: " + _score + "/" + maxScore;
+            }
+        }
     }
+
+    public Text scoreText, timeText;
+
+    private void Awake()
+    {
+        maxScore = Random.Range(minScoreRange, maxScoreRange);
+    }
+
+    private void Update()
+    {
+        SecondsLeft -= Time.deltaTime;
+    }
+
+    public void AddScore(int score)
+    {
+        Score += score;
+    }
+
 }
