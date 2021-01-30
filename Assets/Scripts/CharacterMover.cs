a0115c4e86e8634ee1130140dde345fa16289ceb
Chase O'Connor <chaseoconnor3@comcast.net>
2021-01-29 21:48:49 -0800

    some work going in

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -2,17 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMover : MonoBehaviour
+public class CharacterMover : SingletonPattern<CharacterMover>
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(5, 10)]
+    public float speed = 10f;
+
+    public Transform holdArea, parentTrans;
+    public GameObject grabArea, desiredGrab, grabbedObject;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        parentTrans = transform.parent;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Rotate();
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            Move();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (grabbedObject == null)
+                GrabItem();
+            else
+                ThrowItem();
+        }
+    }
+
+    public void Rotate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit, 1000f, 1 << 8);
+
+        if (hit.collider == null) return;
+
+        transform.LookAt(new Vector3(hit.point.x, 1f, hit.point.z));
+    }
+
+    public void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        parentTrans.position += direction * speed * Time.deltaTime;
+        //Mathf.Clamp(0f, 1000f);
+    }
+
+    public void GrabItem()
+    {
+        desiredGrab = null;
+        grabArea.GetComponent<BoxCollider>().enabled = true;
         
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit, 1000f, 1 << 9);
+
+        if (hit.collider == null) return;
+
+        desiredGrab = hit.collider.gameObject;
+        
+        //Debug.Log("Want to grab " + desiredGrab.name);
+    }
+
+    public void ThrowItem()
+    {
+        grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+        grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+        
+        grabbedObject.transform.parent = null;
+        
+        grabbedObject = null;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == desiredGrab)
+        {
+            other.gameObject.transform.parent = holdArea;
+            
+            other.gameObject.transform.localPosition = Vector3.zero;
+            
+            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            
+            grabbedObject = other.gameObject;
+            
+            grabArea.GetComponent<BoxCollider>().enabled = false;
+            
+            desiredGrab = null;
+        }
     }
 }
