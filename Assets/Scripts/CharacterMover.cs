3fb796bcc14689a105bba0427ad618c97fe341e6
Chase O'Connor <chaseoconnor3@comcast.net>
2021-01-31 17:09:49 -0800

    final shit

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * Author: Chase O'Connor
+ * Date: 1/29/2020
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ * Author: Chase O'Connor
+ * Date: 1/29/2020
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
